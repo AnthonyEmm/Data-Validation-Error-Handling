c35aa2b1a5056e5e43111cc6d8e392afaea7ce8b
Nnaemeka Emesowum <nna.emesowum@gmail.com>
2023-11-25 18:08:43 +0100

    removed the import axios in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
 import Tasks from "./components/Tasks";
 import CreateTasks from "./components/CreateTasks";
 import "./App.css";
